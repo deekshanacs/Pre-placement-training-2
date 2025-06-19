3cc5d7368a39f311a8b7a16da80524e7443bebe5
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-19 17:48:03 +0530

    8-puzzle

---
+++ 19.06.2025/Own Scenario-1.java
@@ -0,0 +1,11 @@
+class PuzzleState {
+    int[][] board;
+    PuzzleState(int[][] board) {
+        this.board = board;
+    }
+    void move(int direction) {
+    }
+    boolean isGoal() {
+        return false;
+    }
+}
