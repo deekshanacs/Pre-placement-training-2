10b5a4d591dc0513d93452432b22b1a2dee74337
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-19 17:49:51 +0530

    Create Own Scenario-3.java

---
+++ 19.06.2025/Own Scenario-3.java
@@ -0,0 +1,24 @@
+public class SudokuSolver {
+    public static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < 9; i++)
+            if (board[row][i] == num || board[i][col] == num ||
+                board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num)
+                return false;
+        return true;
+    }
+    public static boolean solve(int[][] board) {
+        for (int row = 0; row < 9; row++)
+            for (int col = 0; col < 9; col++)
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) return true;
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+        return true;
+    }
+}
