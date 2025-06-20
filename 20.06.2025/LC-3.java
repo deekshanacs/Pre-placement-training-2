a59cda371b818546a3537865e97158da2ad70e01
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-20 19:18:23 +0530

    Create LC-3.java

---
+++ 20.06.2025/LC-3.java
@@ -0,0 +1,6 @@
+class Eligibility {
+    public static void main(String[] args) {
+        int score = 85;
+        System.out.println(score >= 80 ? "Eligible for scholarship" : "Not eligible");
+    }
+}
