39bd8a69f2d8812b516640ef04f2cf4e59364d19
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-20 19:11:59 +0530

    Create Code-chef-1.java

---
+++ 20.06.2025/Code-chef-1.java
@@ -0,0 +1,7 @@
+class Attendance {
+    public static void main(String[] args) {
+        int totalDays = 180, attended = 160;
+        double percentage = (attended * 100.0) / totalDays;
+        System.out.println("Attendance: " + percentage + "%");
+    }
+}
