0f4fa6e5af0f96b2b1236bd7e7ece80815f7f5da
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-20 19:18:47 +0530

    Create LC-5.java

---
+++ 20.06.2025/LC-5.java
@@ -0,0 +1,7 @@
+class Strength {
+    public static void main(String[] args) {
+        int[] classStrength = {30, 28, 35};
+        for (int i = 0; i < classStrength.length; i++)
+            System.out.println("Class " + (i + 1) + ": " + classStrength[i] + " students");
+    }
+}
