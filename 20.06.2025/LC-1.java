13c222c5de2abe43f9bc4834dec527c6f661cee1
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-20 19:14:16 +0530

    Create LC-1.java

---
+++ 20.06.2025/LC-1.java
@@ -0,0 +1,8 @@
+class Marks {
+    public static void main(String[] args) {
+        int m1 = 80, m2 = 75, m3 = 90;
+        int total = m1 + m2 + m3;
+        double percentage = total / 3.0;
+        System.out.println("Total: " + total + ", Percentage: " + percentage);
+    }
+}
