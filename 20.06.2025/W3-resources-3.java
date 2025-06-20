f279cc7889f2958001798de8fe4b6c43cff2fce1
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-20 19:11:29 +0530

    Create W3-resources-3.java

---
+++ 20.06.2025/W3-resources-3.java
@@ -0,0 +1,9 @@
+class Grade {
+    public static void main(String[] args) {
+        int mark = 78;
+        if (mark >= 90) System.out.println("Grade A");
+        else if (mark >= 75) System.out.println("Grade B");
+        else if (mark >= 60) System.out.println("Grade C");
+        else System.out.println("Grade D");
+    }
+}
