6402401b880b56b9ccb7d2edeba63f8564770a61
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-20 19:17:57 +0530

    Create LC-4.java

---
+++ 20.06.2025/LC-4.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class SortStudents {
+    public static void main(String[] args) {
+        int[] marks = {88, 75, 90, 60};
+        Arrays.sort(marks);
+        System.out.println("Sorted Marks: " + Arrays.toString(marks));
+    }
+}
