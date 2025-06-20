60f3b80ee2011d37a7e7f51064575b0fb1e20550
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-20 19:15:00 +0530

    Create Code-chef-4.java

---
+++ 20.06.2025/Code-chef-4.java
@@ -0,0 +1,7 @@
+class FeesStatus {
+    public static void main(String[] args) {
+        int totalFees = 5000, paid = 3000;
+        int balance = totalFees - paid;
+        System.out.println("Balance Due: ₹" + balance);
+    }
+}
