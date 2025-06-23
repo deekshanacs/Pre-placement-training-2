5325494a46fa10aad9509c4e423cf3ae926d1aec
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-23 18:00:18 +0530

    Create W3-9.java

---
+++ 23.06.2025/W3-9.java
@@ -0,0 +1,6 @@
+int rows = 4;
+for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= rows - i; j++) System.out.print("  ");
+    for (int k = 1; k <= i; k++) System.out.print("* ");
+    System.out.println();
+}
