3658cb29d01d4ed7d8ff3d43e0dbcc4e2d6ddad8
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-23 18:01:18 +0530

    Create W3-11.java

---
+++ 23.06.2025/W3-11.java
@@ -0,0 +1,11 @@
+int rows = 4;
+for (int i = 0; i < rows; i++) {
+    int number = 1;
+    for (int j = 0; j < rows - i; j++)
+        System.out.print(" ");
+    for (int k = 0; k <= i; k++) {
+        System.out.print(number + " ");
+        number = number * (i - k) / (k + 1);
+    }
+    System.out.println();
+}
