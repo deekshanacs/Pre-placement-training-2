d43c842321a96135df09989d1a56e98bfb31dca9
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-24 18:02:25 +0530

    Create W3-10.java

---
+++ 24.06.2025/W3-10.java
@@ -0,0 +1,21 @@
+class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+        System.out.println("Singleton instance created.");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        } else {
+            System.out.println("Instance already exists.");
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+    }
+}
