800e6c44549e692b31f3ec6c59ce629a78280dbf
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-25 19:23:38 +0530

    Create HRK-3.py

---
+++ 25.06.2025/HRK-3.py
@@ -0,0 +1,10 @@
+A = set(input().split())
+n = int(input())
+is_strict_superset = True
+for _ in range(n):
+    other_set = set(input().split())
+    if not (A > other_set):  
+        is_strict_superset = False
+        break
+
+print(is_strict_superset)
