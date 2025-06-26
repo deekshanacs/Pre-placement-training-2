7e214256bb6646de20170e2a39789c198c337b48
deekshanacs <145849222+deekshanacs@users.noreply.github.com>
2025-06-26 13:44:10 +0530

    Create GFG-8.py

---
+++ 27.06.2025/GFG-8.py
@@ -0,0 +1,17 @@
+class Solution:
+    def binarysearch(self, arr, k):
+        low = 0
+        high = len(arr) - 1
+        result = -1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] == k:
+                result = mid
+                high = mid - 1  
+            elif arr[mid] < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return result
